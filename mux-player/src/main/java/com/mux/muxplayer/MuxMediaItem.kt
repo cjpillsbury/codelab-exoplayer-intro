0b6ea888787679157f6b4305bbc824d6192d9762
Christian Pillsbury <cpillsbury@mux.com>
2023-08-02 12:54:06 -0700

    feat: stub out and add code for start of potential auto-CMCD solution.

--- mux-player/src/main/java/com/mux/muxplayer/MuxMediaItem.kt
+++ mux-player/src/main/java/com/mux/muxplayer/MuxMediaItem.kt
@@ -4,9 +4,18 @@ import android.net.Uri
 import androidx.media3.common.MediaItem
 import androidx.media3.common.MimeTypes
 
+//object MuxMediaItemFactory {
+//    fun createMuxMediaItem(assetId: String, customDomain: String?): MediaItem {
+//        // TODO: real impl
+//        return MediaItem.Builder().build()
+//    }
+//}
+//fun MediaItem.Builder.setPlaybackId(playbackId: String?): MediaItem.Builder {
+//    return MuxMediaItemBuilder(this).setPlaybackId(…).builder
+//}
+
 // TODO: Will likely need to use extensions here? (CJP)
 typealias MuxMediaItem = MediaItem
-
 
 const val MUX_DOMAIN = "mux.com"
 // TODO: This is an inner class on exo MediaItem.Builder
--- mux-player/src/main/java/com/mux/muxplayer/MuxPlayer.kt
+++ mux-player/src/main/java/com/mux/muxplayer/MuxPlayer.kt
@@ -1,17 +1,24 @@
 package com.mux.muxplayer
 
 import android.content.Context
-import androidx.media3.common.Player
 import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.source.MediaSource
 
 class MuxPlayer(val exoPlayer: ExoPlayer): ExoPlayer by exoPlayer {
     override fun release() {
         exoPlayer.release()
         // Do our own release stuff (NOTE: Doing after based on example order)
+        // Example: clean up mux stats crud here (CJP)
     }
     class Builder(private val context: Context) {
-        private val builder: ExoPlayer.Builder = ExoPlayer.Builder(context)
+        private var builder: ExoPlayer.Builder = ExoPlayer.Builder(context)
 
+        // NOTE: Prioritizing this in POC for one route forward on CMCD support "out of box" (CJP
+        fun setMediaSourceFactory(mediaSourceFactory: MediaSource.Factory): Builder {
+            this.builder = this.builder.setMediaSourceFactory(mediaSourceFactory)
+            // Also do the default cmcd crud here
+            return this
+        }
         fun build(): MuxPlayer {
             println("I AM A MUXOPLAYER RAWR")
             val exoPlayerInstance = builder.build()
